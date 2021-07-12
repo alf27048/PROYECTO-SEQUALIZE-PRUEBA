460034d4b5e35336e4ffddb2a6bc0ddc352d04ed
alf27048 <analaurafarfan@gmail.com>
2021-07-12 18:45:02 -0300

    Listado de productos y pagina de productos

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
 const db = require('../models');
 
 const getProduct = async () => {
-    const product = await db.producto.findAll()
+    const product = await db.producto.findAll({ include: db.extra})
         .then(result => {
             return result;
         });
 
     return product;
 }
+const getExtra = async () => {
+    const Extra = await db.extra.findAll()
+        .then(result => {
+            return result;
+        });
+
+    return Extra;
+}
+
+const getProductById = async (id) => {  //id es el dato que envio en la URL
+    const produc = await db.producto.findByPk(id, {include: db.extra}) //find by primary Key, dame este dato que tiene este id
+           // hace una conexion a la otra tabla y me trae los datos extras
+        .then(result => {
+            return result;
+        });
+        return produc;
+}
+
+
 
 module.exports = {
-    getProduct
+    getProduct,
+    getExtra,
+    getProductById
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const api = require('../api');
+const db = require('../models');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
   const product = await api.getProduct();
   console.log(product);
 
-  //res.render('index', { title: 'Proyecto' });
-  res.send(product); // devuelve un JSON con la informacion
+  res.render('index', { title: 'Proyecto', product });
+  // res.send(product); // devuelve un JSON con la informacion
 });
 
 /* GET de Contacto */
@@ -19,5 +20,20 @@ router.get('/contacto', (req, res) => {
 router.get('/nosotros', (req, res) => {
   res.render('pages/nosotros');
 });
+
+router.get('/extra', async (req, res) => {
+  const Extra = await api.getExtra();
+  console.log(Extra);
+
+  res.send(Extra); // devuelve un JSON con la informacion
+});
+
+router.get('/producto/:id', async (req, res) => {
+  //console.log(req.params);
+  const produc = await api.getProductById(req.params.id);
+  //res.send(produc);
+  res.render('pages/productos', { produc });  // ahi le envio la info
+});
+
 
 module.exports = router;
